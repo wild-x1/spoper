28645eaeae5b492e67cbf2d4ff95ff101226d090
wild-x1 <mwbaih@gmail.com>
2023-09-28 12:57:14 +0700

    fixed error

--- app/core/models.py
+++ app/core/models.py
@@ -11,10 +11,7 @@ class UserManager(BaseUserManager):
         if not email:
             raise ValueError('User must have an email address')
 
-        email = self.normalize_email(email)
-        email = email.lower()
-
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
 
         user.set_password(password)
         user.save(using=self._db)
@@ -34,6 +31,7 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=255)
     email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -14,14 +14,14 @@ class AdminSiteTests(TestCase):
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
-            name='Test User',
+            username='Test User',
         )
 
     def test_users_list(self):
         url = reverse('admin:core_user_changelist')
         res = self.client.get(url)
 
-        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.username)
         self.assertContains(res, self.user.email)
 
     def test_edit_user_page(self):
